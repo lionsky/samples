4968c7d93bfeed0249c37721eaba6f6c24805b07
lionsky <weiliang.lin2@gmail.com>
2017-02-20 17:06:01 +0800

    Create config.js

---
+++ push-messaging-and-notifications/config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAAQYZ2qQo:APA91bEsujqXoS7l_EjLfnak9zuodPW9wkftc-HHwEr0XyVAupeAVyDMV2Cs17u5mL0zoIRLBvPR3os-G8Qmi-7QkickJX97xf0901dMdfMkAcZRf26WPwihHM77kIIS_O1BGKumNFwe'
+};
