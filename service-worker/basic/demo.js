bf318705d5c112e7d3dd9cbdd5b8900801bc0985
lionsky <weiliang.lin2@gmail.com>
2017-02-20 15:07:44 +0800

    Update demo.js

--- service-worker/basic/demo.js
+++ service-worker/basic/demo.js
@@ -1,6 +1,10 @@
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js');
+  navigator.serviceWorker.register('service-worker.js').then (function(registration) {
+    console.log("ServiceWorker registration successful!");
+  }).catch(function(err) {
+    console.log("ServiceWorker registration failed!");
+  }) ;
   console.log("register service worker");
 }
 
